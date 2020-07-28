aa4a68ed9d0e4726d30aee68c0b6870a6b6bb54c
hasianamin <hasianamin14@gmail.com>
2020-07-28 15:52:59 +0700

    menambah function

--- day7/Binary.js
+++ day7/Binary.js
@@ -1,13 +1,16 @@
-let inputan = 50
 let simpan = []
 let sisa = 0
 let i = 0
-while(inputan / 2 > 0){
-    simpan[i] = inputan % 2
-    sisa = Math.floor(inputan/2)
-    inputan = sisa
-    i++
+
+function eksekusi(inputan){
+    while(inputan / 2 > 0){
+        simpan[i] = inputan % 2
+        sisa = Math.floor(inputan/2)
+        inputan = sisa
+        i++
+    }
+    simpan.reverse()
+    let res = simpan.join('')
+    return res
 }
-simpan.reverse()
-let res = simpan.join('')
-console.log(res)
+console.log(eksekusi(105))
