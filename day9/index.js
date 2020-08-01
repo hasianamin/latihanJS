515a36cdff8871b4d8133640279d76d414b11a86
hasianamin <hasianamin14@gmail.com>
2020-08-01 13:16:26 +0700

    intro html dan dom

---
+++ day9/index.js
@@ -0,0 +1,38 @@
+const robin = (bool) => {
+    if(bool){
+
+    }else{
+        return {
+            max : [,,() => {
+                return {
+                    bayu : {
+                        brian : (bool) => {
+                            if(bool){
+                                return 'Hana'
+                            }
+                        }
+                    }
+                }
+            }]
+        }
+    }
+
+}
+
+console.log(robin(false).max[2]()['bayu'].brian(true)) //Hana
+
+//function dgn if else > object bernama max > di object max ada array index 2 berisi object bernama bayu > di dalam object bayu ada method brian 
+
+let max = [ , , [ , {
+    hana : () => {
+        return () => {
+            return (bool) => {
+                if(!bool) return 'hari'
+            }
+        }
+    }
+}]]
+
+
+
+console.log(max[2][1]['hana']()()(false)) //hari
